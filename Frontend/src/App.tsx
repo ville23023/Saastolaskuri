91094ac4b0dfd682e677b5625f890048cd39ee2b
OAnsaharju <otto24002@student.hamk.fi>
2025-04-25 11:04:38 +0300

    first commit

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import "./index.css";
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
